f584b78c248f655fde3f6a8987c3a8cac597178c
Anna Sonchak <kisukeurahara1328@gmail.com>
2024-09-22 16:09:19 +0200

    Created unit tests for CreateSellOrder

--- 15. xUnit/StockAppv2/CRUDTests/StockServiceTests.cs
+++ 15. xUnit/StockAppv2/CRUDTests/StockServiceTests.cs
@@ -63,5 +63,62 @@ namespace CRUDTests
             Assert.True(buy_order_reponse_from_add.BuyOrderId != Guid.Empty);
         }
         #endregion
+
+        #region CreateSellOrder
+
+        [Theory]
+        [InlineData(null, "Microsoft Corp.", "2001-01-01", 100, 9000, typeof(ArgumentNullException))] // Null BuyOrderRequest & null StockSymbol
+        [InlineData("MSFT", "Microsoft Corp.", "2001-01-01", 0, 9000, typeof(ArgumentException))] // Quantity is too low
+        [InlineData("MSFT", "Microsoft Corp.", "2001-01-01", 100001, 9000, typeof(ArgumentException))] // Quantity is too high
+        [InlineData("MSFT", "Microsoft Corp.", "2001-01-01", 100, 0, typeof(ArgumentException))] // Price is too low
+        [InlineData("MSFT", "Microsoft Corp.", "2001-01-01", 100, 10001, typeof(ArgumentException))] // Price is too high
+        [InlineData("MSFT", "Microsoft Corp.", "1999-12-31", 100, 100, typeof(ArgumentException))] // Date is too old
+        public void CreateSellOrder_InvalidParameters_ThrowsException(
+            string stockSymbol,
+            string stockName,
+            string date,
+            uint quantity,
+            double price,
+            Type expectedException)
+        {
+            // Arrange
+            var sellOrderRequest = stockSymbol == null ? null : new SellOrderRequest
+            {
+                StockSymbol = stockSymbol,
+                StockName = stockName,
+                DateAndTimeOfOrder = DateTime.Parse(date),
+                Quantity = quantity,
+                Price = price
+            };
+
+            // Assert
+            Assert.Throws(expectedException, () =>
+            {
+                // Act
+                _stocksService.CreateSellOrder(sellOrderRequest);
+            });
+        }
+
+        [Fact]
+        public void CreateSellOrder_ValidParameters()
+        {
+            // Arrange
+            SellOrderRequest? sell_order_request = new()
+            {
+                StockSymbol = "MSFT",
+                StockName = "Microsoft Corp.",
+                DateAndTimeOfOrder = DateTime.Parse("2001-01-01"),
+                Quantity = 100,
+                Price = 100
+            };
+
+            // Act
+            SellOrderResponse sell_order_reponse_from_add = _stocksService.CreateSellOrder(sell_order_request);
+
+            // Assert
+            Assert.True(sell_order_reponse_from_add.SellOrderId != Guid.Empty);
+        }
+
+        #endregion
     }
 }
