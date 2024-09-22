17995c5e257c04f047a2777e9dc05d87a60b4da4
Anna Sonchak <kisukeurahara1328@gmail.com>
2024-09-22 11:04:18 +0200

    Assignment 27. Finished FinnhubService, started DTOs

---
+++ 15. xUnit/StockAppv2/ServiceContracts/CustomValidators/MaximumDateValidatorAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.CustomValidators
+{
+    public class MaximumDateValidatorAttribute : ValidationAttribute
+    {
+        private readonly DateTime _maximumDate;
+        public MaximumDateValidatorAttribute(string maxDate) 
+        {
+            _maximumDate = DateTime.Parse(maxDate);
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            if ((DateTime)value > _maximumDate)
+            {
+                return new ValidationResult(ErrorMessage ?? $"Date should not be older than {_maximumDate}", [nameof(validationContext.MemberName)]);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ 15. xUnit/StockAppv2/ServiceContracts/DTO/BuyOrderRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceContracts.CustomValidators;
+
+namespace ServiceContracts.DTO
+{
+    public class BuyOrderRequest
+    {
+        [Required(ErrorMessage = "StockSymbol cannot be blank")]
+        public string? StockSymbol { get; set; }
+
+        [Required(ErrorMessage = "StockName cannot be blank")]
+        public string? StockName { get; set; }
+
+        [MaximumDateValidator("2000-01-01")]
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 100000)]
+        public uint Quantity { get; set; }
+
+        [Range(1, 10000)]
+        public double Price { get; set; }
+    }
+}
