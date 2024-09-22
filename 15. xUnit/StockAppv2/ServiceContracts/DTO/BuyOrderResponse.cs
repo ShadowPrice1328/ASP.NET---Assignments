c53d55802b77ae3aa2e696d963a20768b7987a04
Anna Sonchak <kisukeurahara1328@gmail.com>
2024-09-22 11:11:35 +0200

    Finished DTOs

--- 15. xUnit/StockAppv2/ServiceContracts/DTO/BuyOrderResponse.cs
+++ 15. xUnit/StockAppv2/ServiceContracts/DTO/BuyOrderResponse.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace ServiceContracts.DTO
 {
-    internal class BuyOrderResponse
+    public class BuyOrderResponse
     {
+        public Guid? BuyOrderId { get; set; }
+        public string? StockSymbol { get; set; }
+        public string? StockName { get; set; }
+        public DateTime DateAndTimeOfOrder { get; set; }
+        public uint Quantity { get; set; }
+        public double Price { get; set; }
+        public double TradeAmount { get; set; }
     }
 }
--- 15. xUnit/StockAppv2/ServiceContracts/DTO/SellOrderResponse.cs
+++ 15. xUnit/StockAppv2/ServiceContracts/DTO/SellOrderResponse.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace ServiceContracts.DTO
 {
-    internal class SellOrderResponse
+    public class SellOrderResponse
     {
+        public Guid? SellOrderId { get; set; }
+        public string? StockSymbol { get; set; }
+        public string? StockName { get; set; }
+        public DateTime DateAndTimeOfOrder { get; set; }
+        public uint Quantity { get; set; }
+        public double Price { get; set; }
+        public double TradeAmount { get; set; }
     }
 }
