260c9555e648bfbae738411836c7d5c7ec070b63
Anna Sonchak <kisukeurahara1328@gmail.com>
2024-09-24 19:49:11 +0200

    CRUD Demo. Added search functionality

--- 15. xUnit/CRUDExample/Controllers/PersonsController.cs
+++ 15. xUnit/CRUDExample/Controllers/PersonsController.cs
@@ -2,6 +2,7 @@
 using ServiceContracts;
 using ServiceContracts.DTO;
 using Services;
+using System.Reflection;
 
 namespace CRUDExample.Controllers
 {
@@ -12,9 +13,25 @@ namespace CRUDExample.Controllers
         [Route("/")]
         [Route("[controller]")]
         [Route("[controller]/index")]
-        public IActionResult Index()
+        public IActionResult Index(string searchBy, string? searchString)
         {
-            List<PersonResponse> responses = _personsService.GetAllPersons();
+            List<PropertyInfo> properties = typeof(PersonResponse).GetProperties().Skip(1).ToList();
+
+            List<string> personFields = [];
+
+            for (int i = 0; i < properties.Count; i++)
+            {
+                //personFields.Add(properties[i].Name, properties[i].GetValue(responses[i])?.ToString() ?? "null");
+                personFields.Add(properties[i].Name);
+            }
+
+            ViewBag.personFields = personFields;
+
+            ViewBag.currentSearchBy = searchBy;
+            ViewBag.currentSearchString = searchString;
+
+            List<PersonResponse> responses = _personsService.GetFilteredPersons(searchBy, searchString);
+
             return View(responses);
         }
     }
